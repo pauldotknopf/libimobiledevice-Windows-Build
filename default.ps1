76b49e7e8d7384c174d64acd2469eb7909a81963
Paul Knopf <theonlylawislove@gmail.com>
2015-05-31 04:14:36 -0400

    Working on build

--- default.ps1
+++ default.ps1
@@ -3,7 +3,6 @@ Framework '4.5.1'
 properties {
     $base_dir = resolve-path .
     $libimobiledevice_source_dir = "$base_dir\libimobiledevice-source"
-    $libiconv_source_dir = "$base_dir\iconv-source"
     $libplist_source_dir = "$base_dir\libplist-source"
     $configuration = "Release"
 }
@@ -12,15 +11,14 @@ task default -depends clean, checkout, build
 
 task clean {
     delete_directory $libimobiledevice_source_dir
-    delete_directory $libiconv_source_dir
     delete_directory $libplist_source_dir
 }
 
 task checkout {
-    exec { git clone git://git.savannah.gnu.org/libiconv.git $libiconv_source_dir }
-    exec { git clone https://github.com/libimobiledevice/libimobiledevice.git $libimobiledevice_source_dir }
-    exec { git clone https://github.com/libimobiledevice/libplist.git $libplist_source_dir  }
-    
+    # exec { git clone https://github.com/libimobiledevice/libimobiledevice.git $libimobiledevice_source_dir }
+    # exec { git clone https://github.com/libimobiledevice/libplist.git $libplist_source_dir  }
+    exec { git clone https://github.com/Polyfun/libimobiledevice-windows.git $libimobiledevice_source_dir }
+    exec { git clone https://github.com/Polyfun/libplist.git $libplist_source_dir  }
 }
 
 task build {
